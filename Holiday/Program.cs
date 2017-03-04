a9c685a2c1e7bc21dad4229226ed93d3f25fbe9b
kheiakiyama <kheiakiyama@gmail.com>
2017-03-04 16:04:45 +0900

    add dayofweek

--- Holiday/Program.cs
+++ Holiday/Program.cs
@@ -11,15 +11,24 @@ namespace Holiday
 			var start = new DateTime(1984, 11, 15);
 			var end = start.AddYears(50);
 			var span = end - start;
-			var filename = "holiday.csv";
-			using (StreamWriter sw = new StreamWriter(filename))
+			using (StreamWriter sw = new StreamWriter("holiday.csv"))
 			{
-				sw.WriteLine("date,holiday,title");
+				sw.WriteLine("year,month,day,holiday,title");
 				for (var i = 0; i <= span.TotalDays; i++)
 				{
 					var day = start.AddDays(i);
 					var holiday = HolidayUtils.GetHoliday(day);
-					sw.WriteLine($"{day.ToLongDateString()},{(!string.IsNullOrEmpty(holiday)).ToString()},{holiday}");
+					sw.WriteLine($"{day.Year},{day.Month},{day.Day},{(!string.IsNullOrEmpty(holiday)).ToString()},{holiday}");
+				}
+			}
+			using (StreamWriter sw = new StreamWriter("holiday-with-dayofweek.csv"))
+			{
+				sw.WriteLine("year,month,day,dayofweek,week,holiday,title");
+				for (var i = 0; i <= span.TotalDays; i++)
+				{
+					var day = start.AddDays(i);
+					var holiday = HolidayUtils.GetHoliday(day);
+					sw.WriteLine($"{day.Year},{day.Month},{day.Day},{day.DayOfWeek.ToString()},{day.Day / 7 + 1},{(!string.IsNullOrEmpty(holiday)).ToString()},{holiday}");
 				}
 			}
 		}
